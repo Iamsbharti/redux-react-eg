d45b7835dcf04db4e85a7bd58a4314b1e5ce1c72
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-04-14 02:36:10 +0530

    implemented connect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./styles.css";
-
-export default function App() {
+import { connect } from "react-redux";
+import { incrementCounter, decrementCounter } from "./redux/counterActions";
+function App(props) {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter-App</h1>
+      <h2>Counter: {props.count}</h2>
+      <button onClick={props.incrementCounter}>+</button>
+      <button onClick={props.decrementCounter}>-</button>
     </div>
   );
 }
+const mapStateToProps = globalState => ({ count: globalState });
+const mapDispatchToProps = {
+  incrementCounter,
+  decrementCounter
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
